cfc4abd75e3e02ed23d78a2aa5bf70006964179b
Saitgalin Khannan <89251146+SaitgalinKhannan@users.noreply.github.com>
2022-07-12 17:54:53 +0500

    Create Coroutines.kt

---
+++ Coroutines.kt
@@ -0,0 +1,19 @@
+package search
+
+fun main() {
+    val n = readln().toInt() //Number of people
+
+    var lst = mutableListOf<String>()
+
+    println(lst)
+
+
+
+    /*println(
+        if (first_str.split(" ").indexOfFirst { it == second_str } != -1) {
+            first_str.split(" ").indexOfFirst { it == second_str } + 1
+        } else {
+            "Not found"
+        }
+    )*/
+}
