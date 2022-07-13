fbe5879c92f39d2293a4fffbd0f9bbf6e828f942
Saitgalin Khannan <89251146+SaitgalinKhannan@users.noreply.github.com>
2022-07-13 11:34:41 +0500

    Update Functions_as_object.kt

--- Functions_as_object.kt
+++ Functions_as_object.kt
@@ -37,5 +37,19 @@ fun main() {
     
     val originalText = "I don't know... what to say..."
     val textWithoutDots = originalText.filter({ c: Char -> c != '.' }) //lambda
+    
+    It works! First of all, Kotlin infers types of many objects, and here specifying the c type isn't necessary:
+
+    originalText.filter({ c -> c != '.' })
+    //Second, there are situations when the lambda is passed as the last argument. This is the case. Kotlin provides a way to eliminate these bracket sequences ({ }), allowing to write the lambda outside the parentheses:
+
+    originalText.filter() { c -> c != '.' }
+    //If the parentheses are left empty after this operation, you can remove them:
+
+    originalText.filter { c -> c != '.' }
+    //Finally, when there is a single parameter in a lambda, there is an opportunity to skip it. The parameter is available under the it name. The final version of the code that removes dots is this:
+
+    val originalText = "I don't know... what to say..."
+    val textWithoutDots = originalText.filter { it != '.' }
 
 }
